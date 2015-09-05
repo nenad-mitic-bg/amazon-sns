7a34bdd881870ddfd180ab2de450c21e25fab152
shone <shonezlo@gmail.com>
2015-09-05 17:37:57 +0200

    Notification row actions

--- custom-type/ASNS_Notification.php
+++ custom-type/ASNS_Notification.php
@@ -21,6 +21,12 @@ class ASNS_Notification
         ));
     }
 
+    /**
+     * Adds columns to listing page
+     * 
+     * @param array $columns
+     * @return array
+     */
     public static function columns($columns)
     {
         $columns['asns_sent_count'] = 'Sent';
@@ -28,6 +34,12 @@ class ASNS_Notification
         return $columns;
     }
 
+    /**
+     * Prints value of the given custom column
+     * 
+     * @global WP_Post $post
+     * @param string $col_name
+     */
     public static function column_val($col_name)
     {
         global $post;
@@ -46,8 +58,30 @@ class ASNS_Notification
         }
     }
 
+    /**
+     * Adds a "send" link to "post actions" block
+     * 
+     * @global WP_Post $post
+     * @param array $actions
+     * @return array
+     */
+    public static function row_actions($actions)
+    {
+        global $post;
+
+        if ($post->post_type == 'asns_pn') {
+            $actions['asns_pn_send'] = '<a href="#"'
+                    . ' class="asns-send"'
+                    . ' data-id="' . $post->ID . '"'
+                    . '>Send notification</a>';
+        }
+
+        return $actions;
+    }
+
 }
 
 add_action('init', 'ASNS_Notification::register_post_type');
 add_action('manage_asns_pn_posts_columns', 'ASNS_Notification::columns');
 add_action('manage_asns_pn_posts_custom_column', 'ASNS_Notification::column_val');
+add_action('post_row_actions', 'ASNS_Notification::row_actions');
