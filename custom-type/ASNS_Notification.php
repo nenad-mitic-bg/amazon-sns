614a4840ef03615b8279045f12a8aa9fc677f51f
shone <shonezlo@gmail.com>
2015-09-05 17:18:31 +0200

    Notification custom columns

--- custom-type/ASNS_Notification.php
+++ custom-type/ASNS_Notification.php
@@ -21,6 +21,33 @@ class ASNS_Notification
         ));
     }
 
+    public static function columns($columns)
+    {
+        $columns['asns_sent_count'] = 'Sent';
+        $columns['asns_last_sent_at'] = 'Last sent at';
+        return $columns;
+    }
+
+    public static function column_val($col_name)
+    {
+        global $post;
+
+        switch ($col_name) {
+            case 'asns_sent_count':
+                $count = get_post_meta($post->ID, 'sent_count', true);
+                echo $count ? $count . ' times' : 'Not sent';
+                break;
+            case 'asns_last_sent_at':
+                $last_datetime = get_post_meta($post->ID, 'last_sent_at', true);
+                echo $last_datetime ? $last_datetime : 'Never';
+                break;
+            default:
+                echo "Unexpected column $col_name";
+        }
+    }
+
 }
 
 add_action('init', 'ASNS_Notification::register_post_type');
+add_action('manage_asns_pn_posts_columns', 'ASNS_Notification::columns');
+add_action('manage_asns_pn_posts_custom_column', 'ASNS_Notification::column_val');
